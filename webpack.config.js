f3f25d227b67252a5350382ae0c8a901c78ca9b0
TSD0313 <57118279+TSD0313@users.noreply.github.com>
2020-01-14 20:31:42 +0900

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: "development",
+  
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: "./src/main.ts",
+    // ファイルの出力設定
+    output: {
+      //  出力ファイルのディレクトリ名
+      path: `${__dirname}/dist`,
+      // 出力ファイル名
+      filename: "main.js"
+    },
+    module: {
+      rules: [
+        {
+          // 拡張子 .ts もしくは .tsx の場合
+          test: /\.tsx?$/,
+          // TypeScript をコンパイルする
+          use: "ts-loader"
+        }
+      ]
+    },
+    // import 文で .ts や .tsx ファイルを解決するため
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    }
+  };
